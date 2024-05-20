072910ee2889799845dc7e0c52f68fec5eff8ce0
Rendy Christian <rendychristiann@gmail.com>
2024-05-20 22:32:39 +0700

    create stunting detection page component (Rendy)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
+import StuntingDetection from "./components/StuntingDetection"
 function App() {
 
   return (
     <>
-    <h1 className = "pt-36 text-xl font-bold text-emerald-400 text-center mx-auto">Posyandu-Line :D</h1>
+    <StuntingDetection />
     </>
   )
 }
---
+++ frontend/src/components/StuntingDetection.jsx
@@ -0,0 +1,17 @@
+const StuntingDetection = () => {
+  return (
+    <section id="stunting-detection" className="pt-36">
+      <div className="container flex flex-wrap">
+        <div className="w-full">
+          <div className="max-w-xl mx-auto text-center">
+            <h1 className="font-bold text-lime-600 text-4xl mt-1 mb-5 text-center">
+              Stunting Detection Page
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StuntingDetection;
