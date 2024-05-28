6060c07c81c7977f1c52ab3e53fd70777abbe69b
AhmadWildan24 <ahmadwildan3198@gmail.com>
2024-05-28 21:11:46 +0700

    artikel page 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from "./components/Navbar"
-import Home from './components/Home'
-import StuntingDetection from "./components/StuntingDetection"
-import InformasiPosyandu from "./components/InformasiPosyandu"
-import InformasiPosyandu2 from "./components/InformasiPosyandu2"
-import SignUp from "./components/SignUp"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import StuntingDetection from "./components/StuntingDetection";
+import InformasiPosyandu from "./components/InformasiPosyandu";
+import InformasiPosyandu2 from "./components/InformasiPosyandu2";
+import SignUp from "./components/SignUp";
+import Article_Page1 from "./components/Article_Page1";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/stuntingDetection' element={<StuntingDetection />} />
-        <Route path='/informasiPosyandu' element={<InformasiPosyandu />} />
-        <Route path='/informasiPosyandu2' element={<InformasiPosyandu2 />} />
-        <Route path='/signUp' element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/stuntingDetection" element={<StuntingDetection />} />
+        <Route path="/informasiPosyandu" element={<InformasiPosyandu />} />
+        <Route path="/informasiPosyandu2" element={<InformasiPosyandu2 />} />
+        <Route path="/article_Page1" element={<Article_Page1 />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Article_Page1.jsx
@@ -0,0 +1,62 @@
+const Article_Page1 = () => {
+  return (
+    <div className="min-h-screen bg-white flex">
+      <div className="md:w-full mx-10 my-10 text-left">
+        <h1 className="font-poppinsSemiBold text-header1">Informasi Hari Ini</h1>
+        <p className="font-poppins text-body font-reguler">Cari tahu informasi menarik dan penting bagi kesehatan buah hati anda. </p>
+        <div className="grid grid-rows-4 lg:grid-cols-3 sm:grid-cols-1 my-5 gap-y-10 gap-x-12">
+          {/*Item1*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item3*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item4*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item5*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item6*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item4*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item5*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item6*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item4*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item5*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+          {/*Item6*/}
+          <div className="rounded-xl bg-white outline outline-2 h-[231px]">
+            <img src="../assets/Rectangle 35.png" alt="brokoli" className="rounded-lg w-full h-full object-cover" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article_Page1;
