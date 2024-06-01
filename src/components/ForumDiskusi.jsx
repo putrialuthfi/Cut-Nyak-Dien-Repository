3c9d95b6dec1ff7f95453c92c3f2bc8b5db8472d
StannisGummo <irbarangga.business@gmail.com>
2024-06-01 20:15:13 +0700

    added discussion page

--- src/components/ForumDiskusi.jsx
+++ src/components/ForumDiskusi.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function ForumDiskusi() {
-  const questions = [
+  const questions1 = [
     {
       id: 1,
       question: "Apa yang membuat anak GTM?",
@@ -10,31 +10,40 @@ function ForumDiskusi() {
       role: "Ibu Rumah Tangga",
       time: "2 hari yang lalu",
       answers: 10,
-    },
+    }
+  ];
+
+  const questions2 = [
     {
-      id: 2,
-      question: "Tips agar anak doyan makan sayur, dong bu bidan",
+      id: 1,
+      question: "Moms, tips supaya anak rajin makan sayur gimana ya?",
       author: "Ervalsa Dwi Nanda",
       role: "Ibu Rumah Tangga",
       time: "2 hari yang lalu",
       answers: 10,
-    },
+    }
+  ];
+
+  const questions3 = [
     {
-      id: 3,
-      question: "Mengapa anak saya demam ketika selesai imunisasi?",
+      id: 1,
+      question: "Mengapa anak saya sering demam ketika selesai imunisasi?",
       author: "Ervalsa Dwi Nanda",
       role: "Ibu Rumah Tangga",
       time: "2 hari yang lalu",
       answers: 10,
-    },
+    }
+  ];
+
+  const questions4 = [
     {
-      id: 4,
-      question: "Anak saya susah minum obat, gimana ya bu solusinya?",
+      id: 1,
+      question: "Anak saya sulit minum obat, bagaimana solusinya?",
       author: "Ervalsa Dwi Nanda",
       role: "Ibu Rumah Tangga",
       time: "2 hari yang lalu",
       answers: 10,
-    },
+    }
   ];
 
   return (
@@ -48,11 +57,13 @@ function ForumDiskusi() {
           />
           <button className="ml-2 px-5 py-2 bg-[#135D66] text-white rounded-lg text-[15px] text-nowrap">Tulis Pertanyaan Baru</button>
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-4">Semua diskusi</h2>
-          {questions.map((question) => (
-            <div key={question.id} className="border-t border-gray-200 py-4">
-              <h3 className="text-lg font-medium">{question.question}</h3>
+        <h2 className="pb-4">Filter berdasarkan: <span className="font-bold">Semua Diskusi</span></h2>
+
+        {/* <!-- Box 1 --> */}
+        <div className="bg-white p-4 rounded-lg shadow-lg mb-5">
+          {questions1.map((question) => (
+            <div key={question.id} className="py-1">
+              <h3 className="text-lg font-bold">{question.question}</h3>
               <div className="text-sm text-gray-500 mt-1">
                 <span>{question.author}</span> • <span>{question.role}</span> • <span>{question.time}</span>
               </div>
@@ -63,17 +74,66 @@ function ForumDiskusi() {
             </div>
           ))}
         </div>
+
+          {/* <!-- Box 2 --> */}
+        <div className="bg-white p-4 rounded-lg shadow-lg mb-5">
+          {questions2.map((question) => (
+            <div key={question.id} className="py-1">
+              <h3 className="text-lg font-bold">{question.question}</h3>
+              <div className="text-sm text-gray-500 mt-1">
+                <span>{question.author}</span> • <span>{question.role}</span> • <span>{question.time}</span>
+              </div>
+              <div className="flex justify-between items-center mt-2 py-2">
+                <button className="text-teal-500">10 Jawaban</button>
+                <button className="text-teal-500">Balas</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+          {/* <!-- Box 3 --> */}
+        <div className="bg-white p-4 rounded-lg shadow-lg mb-5">
+          {questions3.map((question) => (
+            <div key={question.id} className="py-1">
+              <h3 className="text-lg font-bold">{question.question}</h3>
+              <div className="text-sm text-gray-500 mt-1">
+                <span>{question.author}</span> • <span>{question.role}</span> • <span>{question.time}</span>
+              </div>
+              <div className="flex justify-between items-center mt-2 py-2">
+                <button className="text-teal-500">10 Jawaban</button>
+                <button className="text-teal-500">Balas</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+         {/* <!-- Box 4 --> */}
+         <div className="bg-white p-4 rounded-lg shadow-lg mb-5">
+          {questions4.map((question) => (
+            <div key={question.id} className="py-1">
+              <h3 className="text-lg font-bold">{question.question}</h3>
+              <div className="text-sm text-gray-500 mt-1">
+                <span>{question.author}</span> • <span>{question.role}</span> • <span>{question.time}</span>
+              </div>
+              <div className="flex justify-between items-center mt-2 py-2">
+                <button className="text-teal-500">10 Jawaban</button>
+                <button className="text-teal-500">Balas</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
         <div className="flex justify-between items-center mt-4">
-          <button className="text-teal-500">Terkini</button>
+          <button className="ml-2 px-5 py-2 bg-[#135D66] text-white rounded-lg text-[12px] text-nowrap">Terkini</button>
           <div>
-            <button className="text-teal-500 mx-1">1</button>
+            <button className="text-teal-500 mx-1 underline md:underline-offset-4">1</button>
             <button className="text-teal-500 mx-1">2</button>
             <button className="text-teal-500 mx-1">3</button>
             <button className="text-teal-500 mx-1">4</button>
             <button className="text-teal-500 mx-1">5</button>
             <button className="text-teal-500 mx-1">6</button>
           </div>
-          <button className="text-teal-500">Terlama</button>
+          <button className="ml-2 px-5 py-2 bg-[#135D66] text-white rounded-lg text-[12px] text-nowrap">Terlama</button>
         </div>
       </div>
     </div>
