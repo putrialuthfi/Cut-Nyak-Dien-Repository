c56b05af55e99e8a6bc5e5f65ff0c6eea72c76b8
putrialuthfi <putrialuthfi@gmail.com>
2024-06-12 13:01:04 +0700

    update  button sign in (luthfi)

--- src/components/ButtonSignIn.jsx
+++ src/components/ButtonSignIn.jsx
@@ -7,15 +7,15 @@ function Button({ activeButton, setActiveButton }) {
       <div className="flex justify-center mb-7">
         <button
           onClick={() => setActiveButton('parent')}
-          className={`text-header3 font-bold w-full flex justify-center items-center px-4 py-2 rounded-tl-lg rounded-bl-lg focus:outline-none ${activeButton === 'parent' ? 'bg-primary text-white' : 'bg-third text-primary'}`}
-          style={{ width: '190px', height: '48px' }}>
+          className={`text-header5 font-semibold w-full flex justify-center items-center px-4 py-2 rounded-tl-lg rounded-bl-lg focus:outline-none ${activeButton === 'parent' ? 'bg-primary text-white' : 'bg-third text-primary'}`}
+          style={{ width: '195px', height: '38px' }}>
           Orangtua
         </button>
 
         <button
           onClick={() => setActiveButton('admin')}
-          className={` text-header3 font-bold flex justify-center items-center px-4 py-2 rounded-tr-lg rounded-br-lg focus:outline-none ${activeButton === 'admin' ? 'bg-primary text-white' : 'bg-third text-primary' }`}
-          style={{ width: '190px', height: '48px' }}>
+          className={` text-header5 font-semibold flex justify-center items-center px-4 py-2 rounded-tr-lg rounded-br-lg focus:outline-none ${activeButton === 'admin' ? 'bg-primary text-white' : 'bg-third text-primary' }`}
+          style={{ width: '195px', height: '38px' }}>
           Admin
         </button>
       </div>
