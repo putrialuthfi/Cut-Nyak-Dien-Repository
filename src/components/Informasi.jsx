2a5bf06e767809eeb43b5d5072a8dd746ab561e8
putrialuthfi <putrialuthfi@gmail.com>
2024-06-14 00:55:23 +0700

    update responsive section informasi (luthfi)

--- src/components/Informasi.jsx
+++ src/components/Informasi.jsx
@@ -1,64 +1,67 @@
-import React from 'react'
-import CourseCard from './InfoCard'
-import { courses } from '../data/data'
-import Slider from 'react-slick'
+import React from 'react';
+import CourseCard from './InfoCard';
+import { courses } from '../data/data';
+import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Courses = () => {
-  var settings = {
-    dots:true,
-    infinite:false,
-    speed:500,
-    slidesToShow:2,
-    slidesToScroll:1,
-    responsive:[
-    {
-        breakpoint:600,
-        settings:{
-            slidesToShow:2,
-            slidesToScroll:1,
-            infinite:false,
-            dots:true
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
         }
-    },
-    {
-        breakpoint:600,
-        settings:{
-            slidesToShow:1,
-            slidesToScroll:2,
-            infinite:false,
-            dots:true
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
         }
-    },
-    {
-        breakpoint:400,
-        settings:{
-            slidesToShow:1,
-            slidesToScroll:1,
-            infinite:false,
-            dots:true
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
         }
-    },
+      }
     ]
-}
+  };
+
   return (
     <section className='w-full h-full bg-white p-4'>
-        <div className='md:max-w-[1100px] m-auto max-w-[400px]'>
-            <h1 className='text-3xl font-bold text-center'>Informasi Hari Ini</h1>
-            <p className='text-[#6D737A] pt-2 pb-8 text-center'>Cari tahu informasi menarik dan penting bagi kesehatan buah hati anda. Penuhi kebutuhannya dengan gizi dan nutrisi <br /> yang tercukupi demi tumbuh kembangnya yang optimal.</p>
-          </div>
-          <a href="/article_Page1">
-        <div className='md:max-w-[1100px] m-auto max-w-[400px] gap-5'>
-          <Slider {...settings} className='px-5'>
-            {courses && courses.map((course) =>(
-                <CourseCard key={course.id} title={course.title} desc={course.desc} linkImg={course.linkImg}/>
-            ))}
-            </Slider>
-        </div>
-        </a>
+      <div className='max-w-5xl mx-auto text-center'>
+        <h1 className='text-3xl font-bold'>Informasi Hari Ini</h1>
+        <p className='text-gray-600 pt-2 pb-8'>
+          Cari tahu informasi menarik dan penting bagi kesehatan buah hati Anda. Penuhi kebutuhannya dengan gizi dan nutrisi yang tercukupi demi tumbuh kembangnya yang optimal.
+        </p>
+      </div>
+      <div className='max-w-5xl mx-auto'>
+        <Slider {...settings} className='px-4'>
+          {courses && courses.map((course) => (
+            <a href="/article_Page1" key={course.id}>
+              <CourseCard title={course.title} desc={course.desc} linkImg={course.linkImg} />
+            </a>
+          ))}
+        </Slider>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
