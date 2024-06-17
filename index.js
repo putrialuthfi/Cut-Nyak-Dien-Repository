99302e8040559685fcff3cf9fd293314c4d05b58
putrialuthfi <putrialuthfi@gmail.com>
2024-06-17 14:20:34 +0700

    modified file index.js (luthfi)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import ScheduleRoute from "./routes/ScheduleRoute.js";
+import PsyProfileRoute from "./routes/PsyProfileRoute.js"
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(ScheduleRoute);
+app.use(PsyProfileRoute);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
