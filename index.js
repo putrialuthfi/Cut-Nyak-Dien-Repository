ff7ef8e54681a2cbb25372f1a7fcea9ec1f4cfb9
putrialuthfi <putrialuthfi@gmail.com>
2024-06-17 13:13:06 +0700

    modified file index.js (luthfi)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import InfoJadwalRoute from "./routes/InfoJadwalRoute.js";
+import ScheduleRoute from "./routes/ScheduleRoute.js";
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(InfoJadwalRoute);
+app.use(ScheduleRoute);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
