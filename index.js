e1910544fdf91754b5c41491d387d8d908775397
putrialuthfi <putrialuthfi@gmail.com>
2024-06-17 15:42:58 +0700

    modified file index.js (luthfi)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import ScheduleRoute from "./routes/ScheduleRoute.js";
-import PsyProfileRoute from "./routes/PsyProfileRoute.js"
-import ChildMonRoute from "./routes/ChildMonRoute.js"
+import PsyProfileRoute from "./routes/PsyProfileRoute.js";
+import ChildMonRoute from "./routes/ChildMonRoute.js";
+import StuntingRoute from "./routes/StuntingRoute.js";
 
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(ScheduleRoute);
 app.use(PsyProfileRoute);
 app.use(ChildMonRoute);
+app.use(StuntingRoute);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
